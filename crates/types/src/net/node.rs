47af97ccbb535af29c550a2f00cb44ec3467c18c
Muhamad Awad <muhamad@restate.dev>
2025-02-13 14:16:09 +0100

    Compact cluster status
    Summary:
    Print out a compact cluster status by default
    
    Extended view of the cluster status can still be accessed
    via the `--extra` flag
    
    > There are also some minor fixes to the restatectl because of conflicting short flags across different commands.

--- crates/types/src/net/node.rs
+++ crates/types/src/net/node.rs
@@ -8,7 +8,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, time::Duration};
 
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
@@ -32,4 +32,7 @@ pub struct NodeStateResponse {
     /// Partition processor status per partition. Is set to None if this node is not a `Worker` node
     #[serde_as(as = "Option<serde_with::Seq<(_, _)>>")]
     pub partition_processor_state: Option<BTreeMap<PartitionId, PartitionProcessorStatus>>,
+
+    /// age of node
+    pub age: Duration,
 }
